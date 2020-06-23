4c099cdd94d200770c6e6862ba327f25aedf3e84
tguiraud <timothee.guiraud@insa-rouen.fr>
2020-06-23 21:19:12 +0200

    Ajouts tests queue_char

--- tetris/qc_queue_char.h
+++ tetris/qc_queue_char.h
@@ -9,9 +9,50 @@ typedef struct qc_queue_t {
     int size;
 } qc_queue_t;
 
+/*
+ * Init a new queue, NULL is returned if an error occured during memory
+ * allocation.
+ */
 qc_queue_t* qc_queue_init();
+
+/*
+ * Return the number of elements in 'queue'.
+ * Return code : 
+ *     -1, error queue null pointer
+ */
 int qc_queue_get_size(qc_queue_t* queue);
+
+/*
+ * Add an element to 'queue'.
+ * Return code :
+ *      0, success
+ *     -1, queue null pointer
+ *     -2, memory allocation error
+ */
 int qc_queue_enqueue(qc_queue_t* queue, char data);
+
+/*
+ * Remove the first element of 'queue', its data is stored in 'data'
+ * Return code :
+ *      0, success
+ *     -1, queue null pointer
+ *     -2, no data to dequeue
+ */
 int qc_queue_dequeue(qc_queue_t* queue, char* data);
+
+/*
+ * Stores the data from the first element of 'queue' in 'data'
+ * Return code :
+ *      0, success
+ *     -1, queue null pointer
+ *     -2, no data to peek
+ */
 int qc_queue_peek(qc_queue_t* queue, char* data);
+
+/*
+ * Removes all data from 'queue'
+ * Return code :
+ *      0, success
+ *     -1, queue null pointer
+ */
 int qc_queue_clear(qc_queue_t* queue);
--- tetris/qc_queue_char_test.c
+++ tetris/qc_queue_char_test.c
@@ -6,18 +6,18 @@
 
 qc_queue_t* queue;
 
-int qc_initialize_tests() {
+static int initialize() {
     queue = qc_queue_init();
     if (queue == NULL) return 1;
     return 0;
 }
 
-int qc_cleanup_tests() {
+static int cleanup() {
     free(queue);
     return 0;
 }
 
-void test_qc_queue_get_size() {
+static void test_queue_get_size() {
     int size;
     char unused;
 
@@ -53,6 +53,132 @@ void test_qc_queue_get_size() {
     return;
 }
 
+static void test_queue_enqueue_dequeue() {
+    int size;
+    int return_code;
+    char data;
+
+    // Basic enqueue
+    return_code = qc_queue_enqueue(queue, 'A');
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0);
+    CU_ASSERT_EQUAL(size, 1);
+
+    // Enqueue with null pointer
+    return_code = qc_queue_enqueue(NULL, 'A');
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, -1);
+    CU_ASSERT_EQUAL(size, 1);
+
+    // Basic dequeue
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0);
+    CU_ASSERT_EQUAL(data, 'A');
+    CU_ASSERT_EQUAL(size, 0);
+
+    // Multiple data
+    return_code = qc_queue_enqueue(queue, 'A');
+    CU_ASSERT_EQUAL(return_code, 0);
+    return_code = qc_queue_enqueue(queue, 'D');
+    CU_ASSERT_EQUAL(return_code, 0);
+    return_code = qc_queue_enqueue(queue, '+');
+    CU_ASSERT_EQUAL(return_code, 0);
+    return_code = qc_queue_enqueue(queue, ';');
+    CU_ASSERT_EQUAL(return_code, 0);
+    return_code = qc_queue_enqueue(queue, ' ');
+    CU_ASSERT_EQUAL(return_code, 0);
+    return_code = qc_queue_enqueue(queue, '2');
+    CU_ASSERT_EQUAL(return_code, 0);
+    return_code = qc_queue_enqueue(queue, 'E');
+    CU_ASSERT_EQUAL(return_code, 0);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(size, 7);
+
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0)
+    CU_ASSERT_EQUAL(data, 'A');
+    CU_ASSERT_EQUAL(size, 6);
+
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0)
+    CU_ASSERT_EQUAL(data, 'D');
+    CU_ASSERT_EQUAL(size, 5);
+
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0)
+    CU_ASSERT_EQUAL(data, '+');
+    CU_ASSERT_EQUAL(size, 4);
+
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0)
+    CU_ASSERT_EQUAL(data, ';');
+    CU_ASSERT_EQUAL(size, 3);
+
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0)
+    CU_ASSERT_EQUAL(data, ' ');
+    CU_ASSERT_EQUAL(size, 2);
+
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0)
+    CU_ASSERT_EQUAL(data, '2');
+    CU_ASSERT_EQUAL(size, 1);
+
+    return_code = qc_queue_dequeue(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0)
+    CU_ASSERT_EQUAL(data, 'E');
+    CU_ASSERT_EQUAL(size, 0);
+}
+
+static void test_queue_peek() {
+    int return_code;
+    char data;
+    int size;
+    
+    // Null pointer
+    return_code = qc_queue_peek(NULL, &data);
+    CU_ASSERT_EQUAL(return_code, -1);
+    
+    // No data to peek
+    return_code = qc_queue_peek(queue, &data);
+    CU_ASSERT_EQUAL(return_code, -2);
+    
+    // Peek with one element
+    qc_queue_enqueue(queue, 'A');
+    return_code = qc_queue_peek(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0);
+    CU_ASSERT_EQUAL(data, 'A');
+    CU_ASSERT_EQUAL(size, 1);
+    
+    // Peek with multiple elements
+    qc_queue_enqueue(queue, ' ');
+    qc_queue_enqueue(queue, '2');
+    qc_queue_enqueue(queue, '?');
+    return_code = qc_queue_peek(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0);
+    CU_ASSERT_EQUAL(data, 'A');
+    CU_ASSERT_EQUAL(size, 4);
+    
+    // Peek special char
+    qc_queue_clear(queue);
+    qc_queue_enqueue(queue, '?');
+    return_code = qc_queue_peek(queue, &data);
+    size = qc_queue_get_size(queue);
+    CU_ASSERT_EQUAL(return_code, 0);
+    CU_ASSERT_EQUAL(data, '?');
+    CU_ASSERT_EQUAL(size, 1);
+}
+
 int main(void) {
     CU_ErrorCode error_code;
     CU_pSuite suite_qc_queue_char;
@@ -60,48 +186,13 @@ int main(void) {
     error_code = CU_initialize_registry();
 
     suite_qc_queue_char = CU_add_suite("qc_queue_char",
-            &qc_initialize_tests, &qc_cleanup_tests);
+            &initialize, &cleanup);
 
-    CU_ADD_TEST(suite_qc_queue_char, &test_qc_queue_get_size);
+    CU_ADD_TEST(suite_qc_queue_char, &test_queue_get_size);
+    CU_ADD_TEST(suite_qc_queue_char, &test_queue_enqueue_dequeue);
+    CU_ADD_TEST(suite_qc_queue_char, &test_queue_peek);
 
     CU_console_run_tests();
 
     CU_cleanup_registry();
 }
-
-/* Registry ⊂ Suites ⊂ Tests */
-
-/*
- *  Return CUE_SUCCESS or CUE_NOMEMORY
- *  CU_ErrorCode CU_initialize_registry()
- */
-
-/*
- *  CU_pSuite CU_add_suite(
- *      const char* strName,
- *      CU_InitializeFunc pInit,  no args, returns 0 if all ok
- *      CU_CleanupFunc pClean)    no args, returns 0 if all ok
- *
- *   CUE_SUCCESS suite creation was successful
- *   CUE_NOREGISTRY the registry has not been initialized
- *   CUE_NO_SUITENAME strName was NULL
- *   CUE_DUP_SUITE the suite's name was not unique
- *   CUE_NOMEMORY memory allocation failed
- */
-
-/*
- *  CU_pTest CU_add_test(
- *      CU_pSuite pSuite,       Pointer to the suite containing the test
- *      const char* strName,    Name of the test
- *      CU_TestFunc pTestFunc)  No arg, no return value
- *
- *  CU_ADD_TEST(suite, test) create test and add it to the suite
- */
-
-/*
- *  void CU_console_run_tests()
- */
-
-/*
- *  CU_cleanup_registry()
- */
